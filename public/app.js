0b453b5e4890ccb77943adf1eb22e2be6ce38314
Ana Alcantara <135041722+8098125043@users.noreply.github.com>
2023-12-18 20:50:36 -0500

    Modificaciones

---
+++ public/app.js
@@ -0,0 +1,16 @@
+
+
+async function validarCedula() {
+    const cedula = document.getElementById('cedula').value;
+    const response = await fetch('http://localhost:3000/validar_cedula', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cedula }),
+    });
+
+    const data = await response.json();
+    const resultadoElement = document.getElementById('resultado');
+    resultadoElement.innerText = `La cédula es ${data.esValida ? 'válida' : 'inválida'}.`;
+}
