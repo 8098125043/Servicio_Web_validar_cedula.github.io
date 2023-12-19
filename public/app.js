ee5b768bd0811f7a2d60ec113cedfe7a256f070d
Ana Alcantara <135041722+8098125043@users.noreply.github.com>
2023-12-18 21:17:12 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,11 +1,7 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-const port = process.env.PORT || 3000;
 
 async function validarCedula() {
     const cedula = document.getElementById('cedula').value;
-    const response = await fetch(`http://localhost:${port}/validar_cedula`, {
+    const response = await fetch(`./validar_cedula`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
